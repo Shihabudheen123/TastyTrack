07c21c10bb42e650765575d0236c12f7afb099e6
shihab <shihab96050412@gmail.com>
2024-06-25 08:32:16 +0530

    react

---
+++ Reactjs/Appnested.js
@@ -0,0 +1,36 @@
+//1
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "im an h1 tag"),
+//     React.createElement("h2", {}, "iam an h2 tag"),
+//   ])
+// );
+
+// console.log(parent);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+//2
+
+// const parent = React.createElement(
+//     'div',{id:'parent'},[
+//         React.createElement('div',{id:'child1'},[React.createElement('h1',{},'iam h1 of child 1'),
+//             React.createElement('h2',{},'iam h2 of child 2')]),React.createElement('div',{id:'child2'},
+//                 [React.createElement  ('h3',{},"iam h3 of child 2"),
+//                     React.createElement('h4',{},'iam h4 of child 4')
+              
+//             ])
+//     ]
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(parent)
+
+
+
+
